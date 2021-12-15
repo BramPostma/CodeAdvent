c7d20d680874c7a7947537dc897b6b0da6aff193
BramPostma <bpostma@bol.com>
2021-12-15 10:46:20 +0100

    add 6b

--- day6/advent6.py
+++ day6/advent6.py
@@ -6,9 +6,20 @@ with open('input.txt') as f:
 
 population = np.array(list(map(int,lines[0].split(','))))
 
+count_arr = np.bincount(population)
+amounts = np.append(count_arr,np.array([0,0,0]))
+
+overgangs_matrix = np.eye(8)
+top_row = [0,0,0,0,0,0,1,0]
+overgangs_matrix = np.vstack([top_row,overgangs_matrix])
+last_column = np.eye(9,1)
+overgangs_matrix = np.hstack([overgangs_matrix,last_column])
+
+
 #with open('the-zen-of-python.txt') as f:
 #    for line in f:
 #        print(line)
+
 
 def make_babies(population):
     n_babies = sum(population==0)
@@ -30,3 +41,15 @@ for i in range(80):
     population = cycle(population)
 
 print(population.size)
+
+int_array = population.astype(int)
+count_arr = np.bincount(int_array)
+print(count_arr)
+
+# 6b
+
+for i in range(256):
+    amounts = np.matmul(amounts,overgangs_matrix)
+
+print(amounts)
+print(amounts.sum())
